c0865cdbcfe0982663431e9f36625b61448fcf70
James Errico <jerrico@unifiedsocial.com>
2013-05-25 17:52:12 -0400

    Moved to use urllib to pass arguments in API.make_call(), added get_games API call

---
+++ localtest.py
@@ -0,0 +1,16 @@
+from gamesdb.api import API
+
+gamesdb_api = API()
+platform_list = gamesdb_api.get_platforms_list()
+for platform in platform_list:
+    this_platform = gamesdb_api.get_platform(platform.id)
+    # platforms.append(this_platform)
+    print this_platform.name, '-', this_platform.id
+    this_platform_games = gamesdb_api.get_platform_games(platform.id)
+    for platform_game in this_platform_games:
+        game_results = gamesdb_api.get_game(id=platform_game.id)
+        for game in game_results:
+            print game.id, "-", game.title, " ", game.overview
+    break
+
+
